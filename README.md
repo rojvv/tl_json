700fab6aa9091c1ccd1b52d9f9f17546283f0897
Roj <ez@roj.im>
2023-04-08 19:41:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+> A port of [alik0211/tl-to-json](https://github.com/alik0211/tl-to-json) for
+> Deno
---
+++ mod.ts
@@ -0,0 +1,119 @@
+class Parser {
+  lines: string[];
+  mode: "constructors" | "functions" = "constructors";
+  // deno-lint-ignore no-explicit-any
+  constructors = new Array<any>();
+  // deno-lint-ignore no-explicit-any
+  functions = new Array<any>();
+
+  constructor(tlContent: string) {
+    this.lines = tlContent.toString().split("\n");
+    this.parse();
+  }
+
+  parse() {
+    const { lines } = this;
+
+    for (let line of this.lines) {
+      line = line.replace(";", "").trim();
+
+      if (line === "" || line.indexOf("//") === 0) {
+        continue;
+      }
+
+      this.parseLine(line);
+    }
+  }
+
+  parseLine(line: string) {
+    if (line === "---functions---") {
+      this.mode = "functions";
+      return;
+    }
+
+    if (this.mode === "constructors") {
+      return this.parseConstructor(line);
+    } else if (this.mode === "functions") {
+      return this.parseFunction(line);
+    }
+  }
+
+  parseConstructor(line: string) {
+    const splitedLine = line.split("=");
+
+    const body = splitedLine[0].trim();
+    const type = splitedLine[1].trim();
+
+    const [predicateWithId, ...paramsAsArray] = body.split(" ");
+
+    const [predicate, idAsString] = predicateWithId.split("#");
+    const id = parseInt(idAsString, 16);
+
+    const isVector = predicate === "vector";
+
+    const params = isVector ? [] : paramsAsArray.map((param) => {
+      const [paramName, paramType] = param.split(":");
+
+      return {
+        name: paramName,
+        type: paramType,
+      };
+    });
+
+    this.constructors.push({
+      id,
+      predicate,
+      params,
+      type,
+    });
+  }
+
+  parseFunction(line: string) {
+    const splitedLine = line.split("=");
+
+    const body = splitedLine[0].trim();
+    const type = splitedLine[1].trim();
+
+    const [predicateWithId, ...paramsAsArray] = body.split(" ");
+
+    const [func, idAsString] = predicateWithId.split("#");
+    const id = parseInt(idAsString, 16);
+
+    const params = paramsAsArray
+      .filter((param) => {
+        if (param[0] === "{" && param[param.length - 1] === "}") {
+          return false;
+        }
+
+        return true;
+      })
+      .map((param) => {
+        const [paramName, paramType] = param.split(":");
+
+        return {
+          name: paramName,
+          type: paramType,
+        };
+      });
+
+    this.functions.push({
+      id,
+      func,
+      params,
+      type,
+    });
+  }
+
+  getJS() {
+    return { constructors: this.constructors, functions: this.functions };
+  }
+
+  getJSON() {
+    return JSON.stringify(this.getJS());
+  }
+}
+
+export function parse(tlContent: string) {
+  const parser = new Parser(tlContent);
+  return parser.getJS();
+}
